6afb0ac80fd4b3eb2ae7858f4cca36fb8d73d7a3
Joshua Haertel <josh.haertel@gmail.com>
2018-06-09 21:07:37 -0600

    Clean OpenGarage src files

--- src/OpenGarage/OpenGarage.h
+++ src/OpenGarage/OpenGarage.h
@@ -27,70 +27,94 @@
 #include <FS.h>
 #include <WiFi.h>
 #include <Ticker.h>
+#include <Esp.h>
+#include <esp32-hal-gpio.h>
+
 #include "defines.h"
 
 struct OptionStruct {
-  String name;
-  uint ival;
-  uint max;
-  String sval;
+    String name;
+    byte ival;
+    uint max;
+    String sval;
 };
 
 struct LogStruct {
-  ulong tstamp; // time stamp
-  uint status;  // door status
-  uint dist;    // distance
+    ulong tstamp; // time stamp
+    uint status;  // door status
+    uint dist;    // distance
 };
 
 class OpenGarage {
 public:
-  static OptionStruct options[];
-  static byte state;
-  static byte alarm;
-  static byte led_reverse;
-  static void begin();
-  static void options_setup();
-  static void options_load();
-  static void options_save();
-  static void options_reset();
-  static void restart() { ESP.restart();} //digitalWrite(PIN_RESET, LOW); }
-  static uint read_distance(); // centimeter
-  static byte get_mode()   { return options[OPTION_MOD].ival; }
-  static byte get_button() { return digitalRead(PIN_BUTTON); }
-  static byte get_switch() { return digitalRead(PIN_SWITCH); }
-  static byte get_led()    { return led_reverse?(!digitalRead(PIN_LED)):digitalRead(PIN_LED); }
-  static bool get_cloud_access_en();
-  static void set_led(byte status)   { digitalWrite(PIN_LED, led_reverse?(!status):status); }
-  static void set_relay(byte status) { digitalWrite(PIN_RELAY, status); }
-  static void click_relay() {
-    set_relay(HIGH);
-    delay(options[OPTION_CDT].ival);
-    set_relay(LOW);
-  }
-  static int find_option(String name);
-  static void log_reset();
-  static void write_log(const LogStruct& data);
-  static bool read_log_start();
-  static bool read_log_next(LogStruct& data);
-  static bool read_log_end();
-  static void play_note(uint freq);
-  static void set_alarm(byte ov=0) { // ov = override value
-    if(ov) alarm = ov*10+1;
-    else alarm = options[OPTION_ALM].ival * 10 + 1;
-  }
-  static void reset_alarm() { alarm = 0; }
-  static void reset_to_ap() {
-    options[OPTION_MOD].ival = OG_MOD_AP;
-    options_save();
-    restart();
-  }
-  static void config_ip();
-  static void play_startup_tune();
+    static OptionStruct options[];
+    static byte state;
+    static int alarm;
+    static byte led_reverse;
+
+    static void begin();
+
+    static void options_setup();
+
+    static void options_load();
+
+    static void options_save();
+
+    static void options_reset();
+
+    static void restart() { ESP.restart(); } //digitalWrite(PIN_RESET, LOW); }
+    static uint read_distance(); // centimeter
+    static byte get_mode() { return options[OPTION_MOD].ival; }
+
+    static int get_button() { return digitalRead(PIN_BUTTON); }
+
+    static int get_switch() { return digitalRead(PIN_SWITCH); }
+
+    static byte get_led() { return static_cast<byte>(led_reverse ? (!digitalRead(PIN_LED)) : digitalRead(PIN_LED)); }
+
+    static bool get_cloud_access_en();
+
+    static void set_led(byte status) { digitalWrite(PIN_LED, static_cast<uint8_t>(led_reverse ? (!status) : status)); }
+
+    static void set_relay(byte status) { digitalWrite(PIN_RELAY, status); }
+
+    static void click_relay() {
+        set_relay(HIGH);
+        delay(options[OPTION_CDT].ival);
+        set_relay(LOW);
+    }
+
+    static int find_option(String name);
+
+    static void log_reset();
+
+    static void write_log(const LogStruct &data);
+
+    static bool read_log_start();
+
+    static bool read_log_next(LogStruct &data);
+
+    static bool read_log_end();
+
+    static void play_note(uint freq);
+
+    static void set_alarm(byte ov = 0) { // ov = override value
+        if (ov) alarm = ov * 10 + 1;
+        else alarm = options[OPTION_ALM].ival * 10 + 1;
+    }
+
+    static void reset_to_ap() {
+        options[OPTION_MOD].ival = OG_MOD_AP;
+        options_save();
+        restart();
+    }
+
+    static void config_ip();
+
+    static void play_startup_tune();
+
 private:
-  static ulong read_distance_once();
-  static File log_file;
-  static void button_handler();
-  static void led_handler();
+    static File log_file;
 
 };
 
