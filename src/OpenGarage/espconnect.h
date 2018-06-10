4ff363dd4b046d44697215d1c9249b3ccfc0a777
Joshua Haertel <josh.haertel@gmail.com>
2018-06-09 21:19:25 -0600

    Remove unused defines in espconnet.h

--- src/OpenGarage/espconnect.h
+++ src/OpenGarage/espconnect.h
@@ -23,18 +23,14 @@
 #ifndef _ESP_CONNECT_H
 #define _ESP_CONNECT_H
 
-#define HTML_OK                0x00
 #define HTML_SUCCESS           0x01
 #define HTML_UNAUTHORIZED      0x02
 #define HTML_MISMATCH          0x03
 #define HTML_DATA_MISSING      0x10
 #define HTML_DATA_OUTOFBOUND   0x11
 #define HTML_DATA_FORMATERROR  0x12
-#define HTML_PAGE_NOT_FOUND    0x20
-#define HTML_FILE_NOT_FOUND    0x21
 #define HTML_NOT_PERMITTED     0x30
 #define HTML_UPLOAD_FAILED     0x40
-#define HTML_REDIRECT_HOME     0xFF
 
 #include <WiFi.h>
 #include <mdns.h>
